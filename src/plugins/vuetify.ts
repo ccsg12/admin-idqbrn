0a8e677c38c9f543cbd3cbb6b9bc25d0882bf887
Caio Cesar <ccsg12@hotmail.com>
2022-04-11 18:07:29 -0300

    Cria página de login estabelece alguns padrões de cor e tema

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,6 +4,22 @@ import "vuetify/styles";
 
 // Vuetify
 import { createVuetify } from "vuetify";
+import type { ThemeDefinition } from "vuetify/dist/vuetify";
 
-export default createVuetify();
+const customTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: "#F8FCFC",
+    primary: "#219653",
+  },
+};
+
+export default createVuetify({
+  theme: {
+    defaultTheme: "customTheme",
+    themes: {
+      customTheme,
+    },
+  },
+});
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-container class="full-page d-flex">
+    <v-row justify="center">
+      <v-col class="d-flex flex-column justify-center" cols="12" md="6" xl="4">
+        <div class="login-form-container">
+          <div class="form-header">
+            <h2 class="header-title">Entre no admin</h2>
+            <span class="header-subtitle">Insira seu e-mail e senha</span>
+          </div>
+
+          <v-form>
+            <v-text-field
+              v-model="email"
+              density="compact"
+              label="E-mail"
+              placeholder="joao@email.com"
+              variant="outlined"
+            />
+
+            <v-text-field
+              v-model="password"
+              :append-inner-icon="passwordVisible ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="passwordVisible ? 'text' : 'password'"
+              density="compact"
+              label="Senha"
+              placeholder="********"
+              variant="outlined"
+              @click:append-inner="passwordVisible = !passwordVisible"
+            />
+          </v-form>
+
+          <v-btn
+            block
+            class="app-button"
+            color="primary"
+            rounded="lg"
+            size="large"
+            @click="login"
+            >Entrar
+          </v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import "./styles.scss";
+
+export default defineComponent({
+  name: "LoginView",
+
+  data() {
+    return {
+      email: "",
+      password: "",
+      passwordVisible: false,
+    };
+  },
+
+  methods: {
+    login() {
+      this.$router.push("/");
+    },
+  },
+});
+</script>
